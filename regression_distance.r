ca536a8dbfe17a559ea7e770131a181a6a2a4732
Aleksandrs Berdicevskis <aleksandrs.berdicevskis@gu.se>
2020-10-08 09:57:32 +0200

    Update regression_distance.r

--- regression_distance.r
+++ regression_distance.r
@@ -1,12 +1,9 @@
 options(scipen=999)
 
-#write(paste ("source", "target", "freq_coef", "freq_p", "anim_coef", "anim_p", "base_coef", "base_p", "freq_anim_coef", "freq_anim_p", "freq_base_coef", "freq_base_p", "anim_base_coef", "anim_base_p", "interact_coef", "interact_p", sep = "\t"), file="regression2.tsv")
 library("lmerTest")	
 
 #model <- glmer(correct ~ freq * animacy * baseform * distance + (1 + freq * animacy * baseform * distance | sourcelang) + (1 + freq * animacy * baseform * distance | target), data = results)
 
-
-#EXCLUDE HINDI
 results <- read.csv(file="fullregr.tsv", sep = "\t", header=TRUE, dec=".")
 
 results_noh <- results[results$target != "hi",]
